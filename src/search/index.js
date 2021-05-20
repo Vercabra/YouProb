e3f37ec12a01581b511e1be54199c2d0b5d6cb87
Vercabra <theunikumkid@gmail.com>
2021-05-20 10:50:49 +0300

    Doc fixing

--- src/search/index.js
+++ src/search/index.js
@@ -1,26 +1,74 @@
 require('dotenv').config()
+Promise = require('bluebird')
 const{ google } = require('googleapis')
-const req = 'Slipknot' // request search
-const amount = '5' // how many first videos
+const config =  require('../config/s_request')
+let Queue = require("queue-promise")
+const fs = require("fs")
+const chalk = require("chalk")
+const cliProgress = require('cli-progress')
+const _ = require("lodash")
+const moment = require("moment")
 
 
 
-google.youtube('v3').search.list({
-    key: process.env.YOUTUBE_TOKEN,
-    part: 'snippet',
-    q: req,
-    type: 'video',
-    maxResults: amount,
-}).then((response) => {
-    const{ data } = response
-    data.items.forEach((item) => {
-        google.youtube('v3').videos.list({
-            part: 'snippet,contentDetails,statistics',
-            id: item.id.videoId,
-            key: process.env.YOUTUBE_TOKEN,
-        }).then((response2 => {
-            console.log(`Title: ${item.snippet.title} |  views: ${response2.data.items[0].statistics.viewCount}`)
-        })).catch((err) => console.log(err))
+async function processing () {
+
+    let data1 = []
+
+    const queue = new Queue({
+        concurrent: 10,
+        interval: 2
+    });
+
+    const progressBar = new cliProgress.SingleBar({
+        format: ' {bar} {percentage}% | {value}/{total} | elapsed: {duration_formatted} | estimated: {eta_formatted} ' +
+            'Current Request: '+chalk.green('{req}'),
+        hideCursor: true
     })
-}).catch((err) => console.log(err))
+
+    progressBar.start(config.length,0)
+
+    queue.on("resolve", res => {
+        progressBar.update(data1.length,{req: source.req})
+    })
+
+    queue.on('end', () => {
+        progressBar.stop()
+        fs.writeFileSync(
+            require.resolve("../config/s-request"),
+            JSON.stringify(data, null, " ")
+        )
+    })
+
+    config.forEach(source =>{
+        google.youtube('v3').search.list({
+            key: process.env.YOUTUBE_TOKEN,
+            part: 'snippet',
+            q: source.req,
+            type: 'video',
+            maxResults: source.amount,
+        }).then((response) => {
+            const{ data } = response
+            let result = []
+            Promise.each(data.items, (item) => {
+                return google.youtube('v3').videos.list({
+                    part: 'snippet,contentDetails,statistics',
+                    id: item.id.videoId,
+                    key: process.env.YOUTUBE_TOKEN,
+                }).then((response2 => {
+                    let res = {title: item.snippet.title, views: response2.data.items[0].statistics.viewCount}
+                    result.push(res)
+                })).catch((err) => console.log(err))
+            }).then(() => {source.result = result; source.updatedAt = new Date(); console.log(source); data1.push(source);
+            progressBar.update(data1.length,{req: source.req}); return source})
+        }).catch((err) => console.log(err))
+    })
+
+    queue.start()
+}
+processing()
+
+
+
+
 
